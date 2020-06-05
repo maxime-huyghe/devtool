1941dbe8ec8e3e173e9ec9846cbb58140f6604e3
Maxime Huyghe <maxime.huyghe@etu.u-pec.fr>
2020-06-05 14:45:06 +0200

    init 2

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" m="test" />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: 'App',
+export default Vue.extend({
+  name: "App",
   components: {
     HelloWorld
   }
-}
+});
 </script>
 
 <style>
